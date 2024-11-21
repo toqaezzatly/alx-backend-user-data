0c79e5165792d3d4c0cf812612314e4426774bd8
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-21 13:33:42 +0200

    check

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,5 @@
+# 0x03. User authentication service
+
+## Resources
+- [sqlalchemy declarative mappig](https://docs.sqlalchemy.org/en/13/orm/tutorial.html#declare-a-mapping)
+- [understanding transactions in sqlalchemy](https://coderpad.io/blog/development/understanding-transactions-in-sqlalchemy/)
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-""" SQLAlchemy model named user"""
-
-from sqlalchemy import Column, Integer, String
+"""user schema module"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """ SQLAlchemy model named user"""
+    """User model class"""
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
-
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
